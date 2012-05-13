20c1d769ac5363ca03e28c81b2816e9a9f66879f
opiopan <opiopan@gmail.com>
2012-05-13 16:33:50 +0900

    Initial Commit

---
+++ Pathfinder/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Pathfinder
+//
+//  Created by opiopan on 12/05/13.
+//  Copyright (c) 2012年 opiopan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Pathfinder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Pathfinder
+//
+//  Created by opiopan on 12/05/13.
+//  Copyright (c) 2012年 opiopan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
